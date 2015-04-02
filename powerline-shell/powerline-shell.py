347fc3825dcb2b89b76b0cfa7d9bdbca16cddd1e
Shane Keulen <shane.keulen@gmail.com>
2015-04-02 17:02:38 -0400

    changed some powerlinesss

--- powerline-shell/powerline-shell.py
+++ powerline-shell/powerline-shell.py
@@ -234,21 +234,6 @@ class Color(DefaultColor):
 
 import os
 
-def add_virtual_env_segment():
-    env = os.getenv('VIRTUAL_ENV')
-    if env is None:
-        return
-
-    env_name = os.path.basename(env)
-    bg = Color.VIRTUAL_ENV_BG
-    fg = Color.VIRTUAL_ENV_FG
-    powerline.append(' %s ' % env_name, fg, bg)
-
-add_virtual_env_segment()
-
-
-import os
-
 def add_ssh_segment():
 
     if os.getenv('SSH_CLIENT'):
